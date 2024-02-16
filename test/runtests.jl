9892a06eaff10fd04a8b002c2c3cd6137bfdeeb6
Lisa <lisa-marie@kauck-online.de>
2024-02-16 09:20:55 +0100

    adding ABase.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 using UtilitiesHelpers
 
-@testset "MARLDynamics_in_Julia.jl" begin
+@testset "UtilitiesHelpers.jl" begin
     vec_singular = make_variable_vector(0.9, 5)
     vec_consistent_length = make_variable_vector([0.1, 0.1, 0.1, 0.1, 0.1], 5)
 
@@ -14,12 +14,13 @@ using UtilitiesHelpers
     @test_throws AssertionError make_variable_vector([0.1, 0.1, 0.1, 0.1], 5)
 end
 
-@testset "MARLDynamics_in_Julia.jl" begin
+@testset "UtilitiesHelpers.jl" begin
     Tkk = rand(4,4)
-    Tkk = Tkk/sum(Tkk)
+    Tkk = Tkk./sum(Tkk, dims=2)
 
-    @test sum(Tkk) == 1
-
+    for k ∈ 1:4 @test sum(Tkk, dims=2)[k] .≈ 1 end
+    
     SD_Tkk = compute_stationarydistribution(Tkk)
-    @test sum(SD_Tkk[:, 2:4] .== -10.) == 12
+    @test sum(SD_Tkk .== -10.) ≥ 12
+    @test sum(SD_Tkk[SD_Tkk .> 0]) ≈ 1
 end
