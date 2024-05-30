5637cb379e45001d5176253199da1b985ccdd567
Lisa <lisa-marie@kauck-online.de>
2024-05-30 13:33:51 +0200

    First Try AgentsBase module but needs adaption of struct agentsbase

--- src/Agents/AgentsBase.jl
+++ src/Agents/AgentsBase.jl
@@ -4,117 +4,64 @@
 
 module AgentsBase
 
-using Revise
-using UtilitiesHelpers
+    using Revise
+    using UtilitiesHelpers
 
+    # The agent base class 
+    ## The agent base class contains core methods to compute the strategy-average reward-prediction error. 
+    #### Das Paket enthält Kernmethoden, um den Fehler bei der Vorhersage der durchschnittlichen Belohnung für eine Strategie zu berechnen. Im Kontext von Multi-Agent Reinforcement Learning (MARL) deutet dies darauf hin, dass das Paket Funktionen oder Algorithmen bereitstellt, um den Fehler zwischen den erwarteten und tatsächlichen durchschnittlichen Belohnungen für bestimmte Strategien zu quantifizieren. Der Fehler in der Vorhersage kann eine wichtige Metrik sein, um die Leistung eines MARL-Algorithmus zu bewerten.
 
-# The agent base class 
-## The agent base class contains core methods to compute the strategy-average reward-prediction error. 
-#### Das Paket enthält Kernmethoden, um den Fehler bei der Vorhersage der durchschnittlichen Belohnung für eine Strategie zu berechnen. Im Kontext von Multi-Agent Reinforcement Learning (MARL) deutet dies darauf hin, dass das Paket Funktionen oder Algorithmen bereitstellt, um den Fehler zwischen den erwarteten und tatsächlichen durchschnittlichen Belohnungen für bestimmte Strategien zu quantifizieren. Der Fehler in der Vorhersage kann eine wichtige Metrik sein, um die Leistung eines MARL-Algorithmus zu bewerten.
+    Base.@kwdef mutable struct agentsbase
+        TransitionTensor::AbstractArray{Float64} # transition model of the environment
+        RewardTensor::AbstractArray{Float64}  # reward model of the environment
+        DiscountFactors::Vector{Float64}  # the agents' discount factors
+        use_prefactor::Bool = false # use the 1-DiscountFactor prefactor
+        opteinsum::Bool = true # optimize einsum functions
+        Omega::AbstractArray{Number}
+        gamma::Vector{Number}
+        N, M, Z, Q::Int 
 
+        # number of agents
+        N = size(RewardTensor)[1]  
+        @assert length(size(TransitionTensor)[2:end-1]) == N "Inconsistent number of agents"
+        @assert length(size(RewardTensor)[3:end-1]) == N "Inconsistent number of agents"
 
-@kwdef mutable struct agentsbase
-    TransitionTensor::AbstractArray{Float64} # transition model of the environment
-    RewardTensor::AbstractArray{Float64}  # reward model of the environment
-    DiscountFactors::Vector{Float64}  # the agents' discount factors
-    use_prefactor::Bool = false # use the 1-DiscountFactor prefactor
-    opteinsum::Bool = true # optimize einsum functions
-    Omega::AbstractArray{Number}
-    gamma::Vector{Number}
-    N, M, Z, Q
+        # number of actions for each agent        
+        M = size(TransitionTensor)[2] 
+        @assert isapprox(size(TransitionTensor)[2:end-1], M) "Inconsisten number of actions"
+        @assert isapprox(size(RewardTensor)[3:end-1], M) "Inconsisten number of actions"
 
-    R=RewardTensor
-    T=TransitionTensor
+        # number of states
+        Z = size(TransitionTensor)[1] 
+        Q = size(TransitionTensor)[1] 
+        @assert size(TransitionTensor)[end-1] == Z "Inconsisten number of states"
+        @assert size(RewardTensor)[end-1] == Z "Inconsisten number of states"
+        @assert size(RewardTensor)[2] == Z "Inconsisten number of states"
 
-    # number of agents
-    N = size(R)[1]  
-    @assert length(size(T)[2:end-1]) == N, "Inconsistent number of agents"
-    @assert length(size(R)[3:end-1]) == N, "Inconsistent number of agents"
+        newagentsbase = new(RewardTensor, TransitionTensor, N, M, Z, Q)
 
-    # number of actions for each agent        
-    M = size(T)[2] 
-    @assert isapprox(size(T)[2:end-1], M), "Inconsisten number of actions"
-    @assert isapprox(size(R)[3:end-1], M), "Inconsisten number of actions"
+        # discount factors
+        gamma = make_variable_vector(DiscountFactors, N)
 
-    # number of states
-    Z = size(T)[1] 
-    Q = size(T)[1] 
-    @assert size(T)[end-1] == Z, "Inconsisten number of states"
-    @assert size(R)[end-1] == Z, "Inconsisten number of states"
-    @assert size(R)[2] == Z, "Inconsisten number of states"
+        # use (1-DiscountFactor) prefactor to have values on scale of rewards
+        pre = if use_prefactor ? 1 - gamma : ones(N)
 
-    newagentsbase = new(R, T, N, M, Z, Q)
+        # 'load' the other agents actions summation tensor for speed
+        Omega = OtherAgentsActionsSummationTensor(N,M)
+        has_last_statdist = false
+        _last_statedist = ones(Z) / Z
+          
+    end #of struct
+    #default constrcutor
+    #agentsbase(TransitionTensor, RewardTensor, DiscountFactors;  use_prefactor = false, opteinsum = true) = abase(TransitionTensor, RewardTensor, DiscountFactors, use_prefactor, opteinsum)
 
-    gamma = make_variable_vector(DiscountFactors, N)
-    pre = if use_prefactor ? 1 - gamma : ones(N)
+    #fieldnames(agentsbase)
 
-    function _OtherAgentsActionsSummationTensor(self::agentsbase)
-        # to sum over the other agents and their respective actions using 'einsum'
-        dim = vcat(self.N, # agent i 
-                [self.N for _ in 1:(self.N-1)], #other agents
-                self.M, # action a of agent i
-                [self.M for _ in 1:self.N], # all acts
-                [self.M for _ in 1:(self.N-1)]) #other actions
-        Omega = zeros(Int64, Tuple(dim))
+    function Tss(self::agentsbase, Xisa::AbstractArray)
+        s = 1; # state s
+        sprim = 2; # next state s'
 
-        for index in CartesianIndices(size(Omega))
-            index = Tuple(index)
-            I = index[1]
-            notI = index[2 : self.N-1]
-            A = index[self.N-1]
-            allA = index[self.N : 2*self.N]
-            notA = index[2*self.N:end]
-
-            if length(unique(collect(Iterators.flatten((I, notI))))) == self.N
-                # all agents indices are different
-
-                if A == allA[I]
-                    #action of agent i equals some other action
-                    cd = allA[1:I] + allA[I+1:end] #other actions
-                    areequal = [cd[k] == notA[k] for k in range(1,self.N-1)]
-                    if all(areequal)
-                        index = CartesianIndex(index)
-                        Omega[index] = 1
-                    end
-                end
-            end
-        end #of for loop
-
-    end #end of function
-    
-    
-end #of struct
-
-for index in CartesianIndices(size(Omega))
-    index = Tuple(index)
-    I = index[1] 
-    notI = index[2 : N-1]
-    print(notI, " next ") 
-end
-
-#default constrcutor
-#agentsbase(TransitionTensor, RewardTensor, DiscountFactors;  use_prefactor = false, opteinsum = true) = abase(TransitionTensor, RewardTensor, DiscountFactors, use_prefactor, opteinsum)
-
-fieldnames(agentsbase)
-
-
-mutable struct MeinStruct
-    p::Float64
-    n::Int64
-    f::Function
-end
-
-# Konstruktor für das Struct
-function MeinStruct(; p::Float64 = 2.0, n::Int64 = 4, f::Function = x -> x + p)
-    return MeinStruct(p, n, f)
-end
-
-# Verwendung des Structs
-mein_objekt = MeinStruct(p=7)
-ergebnis = mein_objekt.f(11)  # Ergebnis: 18
-
-
+        
+    end
 
 end #end of module
-
-  
